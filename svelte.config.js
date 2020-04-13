ee097ab655c50a26807cae56cf99b57473d6ad52
Ben McGarvey <ben@sidekicker.com.au>
2020-04-13 21:44:29 +1000

    Add files via upload

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+const sveltePreprocess = require('svelte-preprocess');
+module.exports = {
+  preprocess: sveltePreprocess({
+    babel: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            loose: true,
+            modules: false,
+            targets: {
+              esmodules: true,
+            },
+          },
+        ],
+      ],
+    },
+  }),
+};
